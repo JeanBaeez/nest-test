f3e15db4b4c1ae974e5f677676dae09bbe47f279
JeanBaeez <jlbaez44@gmail.com>
2023-12-02 00:55:57 -0500

    adding movies

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
   }
-
-  getHello2(): string {
-    return 'Hello World 2!';
+  async getMovies() {
+    const prisma = new PrismaClient();
+    const data = await prisma.movies.findMany({
+      take: 1000,
+    });
+    return await data;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  type NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  app.enableCors();
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
